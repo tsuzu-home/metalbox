d0c05f1d4084eb0907c16bd618c7a1e115379df2
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2024-05-11 02:44:36 +0900

    Refactor

--- nodes.tf
+++ nodes.tf
@@ -1,19 +1,3 @@
-# // Default matcher group for machines
-# resource "matchbox_group" "default" {
-#   name    = "default"
-#   profile = matchbox_profile.flatcar-install.name
-# }
-
-# // Match install stage Flatcar Linux machines
-# resource "matchbox_group" "stage-1" {
-#   name    = "worker"
-#   profile = matchbox_profile.worker.name
-
-#   selector = {
-#     os = "installed"
-#   }
-# }
-
 module "laurel" {
   source   = "./k0snode"
   matchbox_http_endpoint = var.matchbox_http_endpoint
